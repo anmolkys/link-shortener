13cc74c5419b38fbaf7746e4f0a2146fe54e7e0c
anmolkys <feimoxin@outlook.com>
2023-10-20 09:34:22 +0530

    Prod ready

--- server.js
+++ server.js
@@ -6,18 +6,20 @@ const Link = require("./models/link");
 const { default: mongoose } = require("mongoose");
 
 app.use(express.json());
-
-mongoose.connect(process.env.MONGO_PASS).then(()=>{console.log("DB connected")});
+app.use(cors());
+const Port=5000
+mongoose.connect("mongodb+srv://krisiscool433:kKJhzHPqEMQZHER6@cluster0.0krqcac.mongodb.net/?retryWrites=true&w=majority").then(()=>{console.log("DB connected")});
 
 app.post("/i",async (req,res)=>{
     const longURL=await req.body?.longURL;
     const shortURL = Math.random().toString(32).substring(7);
-    const pass="localhost:5000/i/"+ shortURL;
+    const pass=`localhost:${Port}/i/`+ shortURL;
+    console.log(pass);
     //db storage
     const link = new Link({shortURL,longURL})
     await link.save();
 
-    res.json({shortURL})
+    res.json({shortURL,pass})
 })
 
 app.get("/i/:short",async (req,res)=>{
@@ -27,8 +29,7 @@ app.get("/i/:short",async (req,res)=>{
     if(!link){
         return res.sendStatus(404);
     }
-    setTimeout(()=>{res.status(301).redirect(link.longURL);},5000)
+    res.status(301).redirect(link.longURL);
     
 })
-const Port=5000
 http.listen(process.env.PORT || Port ,()=>{console.log(`Listening on ${Port}`)})
